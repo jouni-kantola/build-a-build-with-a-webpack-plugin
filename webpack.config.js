c52a6a0894fb7918de61af6c385b70a2ee0d58a0
Jouni Kantola <jouni@kantola.se>
2017-05-12 00:07:23 +0200

    Config created

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.join(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015']
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}
+		]
+	},
+
+	plugins: []
+};
